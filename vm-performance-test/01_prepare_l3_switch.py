dcc5f4ba9954d52ba543cfc9474b9f7785fc5fe6
Namgon Kim <day10000@gmail.com>
2015-04-23 14:24:00 +0900

    Modify red_ip to use 252 if t_id is bigger than 49

--- vm-performance-test/01_prepare_l3_switch.py
+++ vm-performance-test/01_prepare_l3_switch.py
@@ -3,7 +3,15 @@ import sys, ConfigParser
 path="/root/workspace/arista_sw_mgmt/lib"
 if path not in sys.path:
     sys.path.append(path)
-    
+
+#------------------------------------------------------------------------------
+# README
+#------------------------------------------------------------------------------
+# main 함수 안의 r_start와 r_end값에 설정할 test_id의 값을 지정한다.
+# 주의할 점은 r_end -1 값 까지만 적용된다는 것이다.
+# 즉 1부터 100까지 하려면 r_start=1, r_end=101 을 입력해야 한다. 
+#------------------------------------------------------------------------------
+
 import sys
 from utils import Console
 from arista_rpc import SwitchConfig, Interface, Vlan
@@ -16,8 +24,13 @@ def add_vlan(cfg, t_id, uplink):
     print "vlan_range= %s" % vid
     uplink.add_trunk_vlan(vid)
     
-    ip_ ="211.196.251.%d/30" % ((t_id * 4) + 1)
-    gw_ ="211.196.251.%d" % ((t_id * 4) + 2)
+    if t_id < 50:
+        red_ip_c=251
+    else:
+        red_ip_c=252
+        
+    ip_ ="211.196.%d.%d/30" % (red_ip_c, (t_id * 4) + 1)
+    gw_ ="211.196.%d.%d" % (red_ip_c, (t_id * 4) + 2)
     net ="211.100.%d.0/24" % (t_id)
     
     print "vid= %s" % vid
@@ -35,7 +48,9 @@ def main():
 
     uplink = Interface("eth35")
     
-    for x in range(41, 51):
+    r_start=51
+    r_end=101
+    for x in range(r_start, r_end):
         add_vlan(cfg, x, uplink)
         
     #Console().log(" ----------------------------------------------------")
